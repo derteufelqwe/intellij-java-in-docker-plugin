9c9f464b8037de66b86685d3eb423c10c88c23f0
GitHub Action <action@github.com>
2021-12-20 08:49:55 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/derteufelqwe/intellijjavaindockerplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.derteufelqwe.intellijjavaindockerplugin.services
+
+import com.github.derteufelqwe.intellijjavaindockerplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
